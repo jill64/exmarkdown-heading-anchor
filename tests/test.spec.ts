54659bd9bd878f5c02eba0bc39e135df2740808e
jill64 <intents.turrets0h@icloud.com>
2023-11-21 02:13:21 +0900

    fix: test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,30 +3,23 @@ import { expect, test } from '@playwright/test'
 test('Test', async ({ page }) => {
   await page.goto('/')
 
-  await expect(page.getByRole('heading', { name: 'Heading1' })).toHaveAttribute(
-    'id',
-    'anchor-Heading1'
-  )
-
-  await expect(page.getByRole('heading', { name: 'Heading2' })).toHaveAttribute(
-    'id',
-    'anchor-Heading2'
-  )
-
-  await expect(page.getByRole('heading', { name: 'Heading3' })).toHaveAttribute(
-    'id',
-    'anchor-Heading3'
-  )
+  await expect(
+    page.getByRole('heading', { name: 'Heading1', exact: true })
+  ).toHaveAttribute('id', 'anchor-Heading1')
 
   await expect(
-    page.getByRole('heading', { name: 'Heading4' })
-  ).not.toHaveAttribute('id', 'anchor-Heading4')
+    page.getByRole('heading', { name: 'Heading2', exact: true })
+  ).toHaveAttribute('id', 'anchor-Heading2')
+
+  await expect(
+    page.getByRole('heading', { name: 'Heading3', exact: true })
+  ).toHaveAttribute('id', 'anchor-Heading3')
 
   const textarea = page.getByPlaceholder('Markdown')
 
   await textarea.fill('# Modified Heading1')
 
   await expect(
-    page.getByRole('heading', { name: 'Modified Heading1' })
+    page.getByRole('heading', { name: 'Modified Heading1', exact: true })
   ).toHaveAttribute('id', 'anchor-Modified Heading1')
 })
